a749ed15a078c5cbab8cc6733a7a0c2d74a10abc
stefvangils <saa.vangils@student.fontys.nl>
2018-03-15 12:10:16 +0100

    working version

---
+++ GTO Multiplayer/Assets/MultiplayerModule/TurnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour {
+    public List<GameObject> players;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void NextTurn()
+    {
+        if(players[0].activeSelf)
+        {
+            players[1].SetActive(true);
+            players[0].SetActive(false);
+        }
+        else
+        {
+            players[0].SetActive(true);
+            players[1].SetActive(false);
+        }
+        
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
