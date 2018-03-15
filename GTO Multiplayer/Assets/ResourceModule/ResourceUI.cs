7afc2b2155f38e5ccea97323532a2ecb1a5e99ce
stefvangils <saa.vangils@student.fontys.nl>
2018-03-15 12:35:49 +0100

    complete working version

--- GTO Multiplayer/Assets/ResourceModule/ResourceUI.cs
+++ GTO Multiplayer/Assets/ResourceModule/ResourceUI.cs
@@ -30,35 +30,40 @@ public class ResourceUI : MonoBehaviour
         UpdateUI();
     }
 
+    //public void UpdateUI()
+    //{
+    //    // We could just set Value.text to resource.Quantity and be done, but in this
+    //    // version of the UI we made the visual number to increase until it reaches
+    //    // the actual number of the model. To do this we use a Coroutine.
+    //    if (valueTicker != null)
+    //    {
+    //        StopCoroutine(valueTicker);
+    //    }
+    //    valueTicker = StartCoroutine(TickVisualValue(resource.GetQuantity()));
+    //}
+    
+    //IEnumerator TickVisualValue(int target)
+    //{        
+    //    while (cachedValue != target)
+    //    {
+    //        if (cachedValue < target)
+    //        {
+    //            cachedValue++;
+    //        }
+    //        else
+    //        {
+    //            cachedValue--;
+    //        }
+    //        Value.text = cachedValue.ToString();
+    //        // The coroutine will run through this code until it returns a yield.
+    //        // Then, it will wait until the next frame and continue from where it stopped.
+    //        // In this case it will change the value by one every frame until current and target are the same.
+    //        yield return null; 
+    //    }
+    //}
+
     public void UpdateUI()
     {
-        // We could just set Value.text to resource.Quantity and be done, but in this
-        // version of the UI we made the visual number to increase until it reaches
-        // the actual number of the model. To do this we use a Coroutine.
-        if (valueTicker != null)
-        {
-            StopCoroutine(valueTicker);
-        }
-        valueTicker = StartCoroutine(TickVisualValue(resource.GetQuantity()));
-    }
-    
-    IEnumerator TickVisualValue(int target)
-    {        
-        while (cachedValue != target)
-        {
-            if (cachedValue < target)
-            {
-                cachedValue++;
-            }
-            else
-            {
-                cachedValue--;
-            }
-            Value.text = cachedValue.ToString();
-            // The coroutine will run through this code until it returns a yield.
-            // Then, it will wait until the next frame and continue from where it stopped.
-            // In this case it will change the value by one every frame until current and target are the same.
-            yield return null; 
-        }
+        Value.text = resource.GetQuantity().ToString();
     }
 }
