334cb364b47383d062761d672aa812b917e5e60c
stefvangils <saa.vangils@student.fontys.nl>
2018-03-15 13:11:47 +0100

    setposition
    toegevoegd dat je de positie kan zetten van de units

--- GTO Multiplayer/Assets/GridModule/UnitFactory.cs
+++ GTO Multiplayer/Assets/GridModule/UnitFactory.cs
@@ -1,20 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitFactory : MonoBehaviour
 {
     public Unit Prototype;
     public Map Map;
+    public Text x;
+    public Text y;
 
 
     public List<ResourceCost> Costs;
     
     // Temporary until we figure out a better way to decide where to spawn.
-    public Vector2Int SpawnCoordinate;
+    private Vector2Int SpawnCoordinate;
 
     public void SpawnUnit()
     {
-
+        SpawnCoordinate.x = Convert.ToInt32(x.text);
+        SpawnCoordinate.y = Convert.ToInt32(y.text);
         bool canAfford = true;
         for (int i = 0; i < Costs.Count; i++)
         {
